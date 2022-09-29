79a5027490da8718e6007b0fd48512563ee65b30
9EED <zeghdns@gmail.com>
2022-09-29 21:26:45 +0100

    update

--- superbox-3.0.py
+++ superbox-3.0.py
@@ -4,95 +4,140 @@ from pyray import *
 from time import *
 
 Fail = 0
-Border_x = 1
-Border_y = 2
-Success = 3
-Neighbor = 4
+Success = 1
+Neighbor = 2
 
 # cool idea: temperature raised by collision
 
-class Cell :
+# fuck you past me implement shit 
+
+class Cell:
     def __init__(self, color):
         self.color = color
-        self.vx = random() - .5
-        self.vy = random() - .5
+        self.vx = 0
+        self.vy = 0
+        self.gravity = 0
+        self.mass = 1
+        self.bounce = 1
+
+class Sand(Cell):
+    def __init__(self):
+        self.color = Color(
+            randint(210, 220),
+            randint(160, 175),
+            randint(60, 65),
+            255
+        )
+        self.vx = 0
+        self.vy = 0
+        self.gravity = 1
+        self.mass = 1
+        self.bounce = 0.02
+
+class Stone(Cell):
+    def __init__(self):
+        v = randint(75, 90)
+        self.color = Color(
+            v,
+            v,
+            v + randint(0, 10),
+            255
+        )
+        self.vx = 0
+        self.vy = 0
+        self.gravity = -1
+        self.mass = 500
+        self.bounce = 0.005
+
 class Camera:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-def attempt_swap(world, x, y, dx, dy):
-    if dx == 0 and dy == 0: return Fail
-    if y + dy == world_height: return Border_y
-    if y + dy == -1: return Border_y
-    if x + dx == world_width: return Border_x
-    if x + dx == -1: return Border_x
-    if world[y+dy][x+dx] is not None: return Neighbor
+def attempt_swap(x, y, dx, dy):
+    if x == dx and y == dy: return Fail
+    if world[dy][dx] is not None: return Neighbor
 
-    world[y+dy][x+dx] = cell
+    world[dy][dx] = world[y][x]
     world[y][x] = None
     return Success
 
 width = 800
 height = 600
-scale = 10
-world_width = 80
-world_height = 60
+world_width = 500
+world_height = 500
+scale = 5
+color_mode = 0
+wrap = True
+wind = 0
+max_wind = 0.008
 
+gravity = 0.05
 
 world = []
 for y in range(world_height):
     world.append([])
     for x in range(world_width):
-        #color = Color(int(y/world_height*255), int(x/world_width*255), randint(0,255),255)
-        color = Color(randint(0, 255), randint(0, 255), randint(0, 255), 255)
-        if random() < 0.6:
-            world[y].append(Cell(color))
-        else: world[y].append(None)
+        world[y].append(None)
 camera = Camera(0, 0)
 
 init_window( width, height, "superbox 3.0")
 set_target_fps(50)
+render_texture = load_render_texture(world_width, world_height)
 while not window_should_close():
 
     # ---- input ----
+
     #   navigation
     if is_mouse_button_down(MOUSE_BUTTON_LEFT):
-        camera.x += get_mouse_delta().x / scale
-        camera.y += get_mouse_delta().y / scale
+        camera.x += get_mouse_delta().x
+        camera.y += get_mouse_delta().y
     if get_mouse_wheel_move() > 0:
-        if scale <= 50:
-            scale += 1 # i shouldn't be adding i should be multiplying
+        if scale <= 100:
+            scale += 1
             camera.x += (width/scale - width/(scale - 1)) / 2
             camera.y += (height/scale - height/(scale - 1)) / 2
     if get_mouse_wheel_move() < 0:
-        if scale >= 5:
+        if scale > 1:
             scale -= 1
             camera.x += (width/scale - width/(scale + 1)) / 2
             camera.y += (height/scale - height/(scale + 1)) / 2
+    if is_key_pressed(KEY_TAB):
+        color_mode += 1
+        color_mode %= 2
+
     #   interaction
     if is_mouse_button_down(MOUSE_BUTTON_RIGHT):
-        x = int(get_mouse_x() / scale - camera.x + (random()*4-2))
-        y = int(get_mouse_y() / scale - camera.y + (random()*4-2))
-        if x >= 0 and x < world_width and y >= 0 and y < world_height:
-            if is_key_down(KEY_LEFT_SHIFT):
-                world[y][x] = Cell(YELLOW)
-                world[y][x].vx = 0
-                world[y][x].vy = -1.5
-            elif is_key_down(KEY_LEFT_CONTROL):
-                world[y][x] = None
-            else:
-                world[y][x] = Cell(GRAY)
-                world[y][x].vx = 0
-                world[y][x].vy = 0
+        x = int((get_mouse_x() - camera.x ) / scale) % (world_width - 1)
+        y = int((- get_mouse_y() + camera.y ) / scale - 1) % (world_height - 1)
+        if is_key_down(KEY_LEFT_SHIFT):
+            world[y][x] = Sand()
+            world[y][x].vy = -.9
+        elif is_key_down(KEY_LEFT_CONTROL):
+            world[y][x] = None
+        else:
+            world[y][x] = Stone()
 
     # ---- simulation ----
+    total_energy = 0
+    # wind speed
+    wind += random()*0.00025-0.000125
+    if wind > max_wind: wind = max_wind
+    if wind < -max_wind: wind = -max_wind
+    wind_side = -1
+    if wind < 0: wind_side = 1
+
     for y in range(world_height):
+        if world[y].count(None) == world_width: continue
         for x, cell in enumerate(world[y]):
             if cell is None: continue
-            # velocity from surroundings
-            if cell.vy < .5:
-                cell.vy += 0.01
+            total_energy += cell.vx + cell.vy
+
+            #gravity and wind
+            if cell.vy > -.5:
+                cell.vy -= gravity * cell.gravity 
+            if world[y][(x+wind_side)%(world_width-1)] is None:
+                cell.vx += wind / cell.mass
 
             # delta from velocity
             dx, dy = 0, 0
@@ -100,47 +145,67 @@ while not window_should_close():
             if cell.vx < 0: dx *= -1
             if abs(cell.vy) > random(): dy = 1
             if cell.vy < 0: dy *= -1
+            dx += x
+            dy += y
+
+            if wrap:
+                dx %= world_width
+                dy %= world_height
 
             # swapping stuff and collision reactions
-            state = attempt_swap(world, x, y, dx, dy)
+            state = attempt_swap(x, y, dx, dy)
             if state == Neighbor:
-                # these ratios and the variation will depend on the bouncyness and absorption of the cells
-                world[y+dy][x+dx].vx += cell.vx * .5 + (random()*.1 - .05) # adding some randomisation to immitate particles not colliding perfectly centered but still in a pixely way
-                world[y+dy][x+dx].vy += cell.vy * .5 + (random()*.1 - .05)
-                cell.vx *= 0.4
-                cell.vy *= 0.4
-            if state == Border_x:
-                cell.vx *= -1
-            if state == Border_y:
-                cell.vy *= -1
+                ratio = cell.mass / (cell.mass + world[dy][dx].mass)
+                fx = cell.vx * cell.mass
+                fy = cell.vy * cell.mass
+                cell.vx = - fx * ratio / cell.mass * cell.bounce
+                cell.vy = - fy * ratio / cell.mass * cell.bounce
+                world[dy][dx].vx  += fx * (1 - ratio) / world[dy][dx].mass
+                world[dy][dx].vy += fy * (1 - ratio) / world[dy][dx].mass
+                #print(ratio)
 
+            #if cell.vx > 1: cell.vx = 1
+            #if cell.vy > 1: cell.vy = 1
+
+    #print(total_energy)
     # ---- rendering ----
+    #       render the whole world in a texture
+    begin_texture_mode(render_texture)
+    clear_background(Color(0, 0, 0, 0))
+    
+    for y in range(world_height):
+        if world[y].count(None) == world_width: continue
+        for x in range(world_width):
+            if world[y][x] is None: continue
+
+            cell = world[y][x]
+
+            if color_mode == 0: 
+                c = cell.color
+            if color_mode == 1: 
+                v = int(min(sqrt(cell.vx**2 + cell.vy**2) * cell.mass ** 0.5 * 230, 230)) + 255 - 230   
+                c = Color(v, v, v, 255)
+            draw_pixel(x, y, c)
+    end_texture_mode()
+    
     begin_drawing()
-    clear_background(Color( 15, 15, 40, 255))
-    for y in range(-1, int(height / scale)+1):
-        world_y = y - int(camera.y)
-        if camera.y < 0: world_y += 1
-        if world_y < 0: continue
-        if world_y >= world_height: break
-        if world[world_y].count(None) == world_width: continue
-        for x in range(-1, int(width / scale)+1):
-            world_x = x - int(camera.x)
-            if camera.x < 0: world_x += 1
-            if world_x < 0: continue
-            if world_x >= world_width: break
-            if world[world_y][world_x] is None: continue
+    clear_background(WHITE)
+    draw_rectangle_gradient_v(0, 0, width, height, Color(153, 218, 255, 255), Color(84, 193, 255, 255))
 
-            cell = world[world_y][world_x]
-            if cell.vx == 0 and cell.vy == 0: r = 0; b = 0
-            else:
-                r = int(abs(cell.vx) / 1 ** 3 * 255) % 255
-                b = int(abs(cell.vy) / 1 ** 3 * 255) % 255
-            c = Color(r, 50, b, 255)
-            #c = cell.color
-            draw_rectangle(int((x + camera.x%1)*scale), int((y + camera.y%1)*scale), scale, scale, c)
+    #       tile the texture to fill the screen
+    xs = world_width * scale
+    ys = world_height * scale
+    for y in range(-1, int(height/xs) + 1):
+        for x in range(-1, int(width/ys) + 1):
+            x1 = x * xs + camera.x % xs
+            y1 = y * ys + camera.y % ys
+            draw_texture_ex(render_texture.texture, Vector2(x1, y1), 0, scale, WHITE)
 
+    # HUD
     draw_rectangle(int(width/2-1), int(height/2-7), 1, 14, WHITE)
     draw_rectangle(int(width/2-7), int(height/2-1), 14, 1, WHITE)
+    draw_rectangle(int((wind/max_wind/2+0.5)*.95*width), 5, 2, 30, WHITE)
+    
     draw_fps(3, 3)
     end_drawing()
 
